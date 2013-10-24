206134390fb2c10d664c3764c86bb83471063a2d
Mathias Goyheneche <mathias.goyheneche@gmail.com>
2013-10-24 18:31:07 +0200

    sending image to tumblr

--- app/workers/hard_worker.rb
+++ app/workers/hard_worker.rb
@@ -8,10 +8,14 @@ class HardWorker
     puts "hello"
     puts name
 
-    # system "killall PTPCamera"
-    # system "gphoto2 --capture-image-and-download"
-    # system "ls"
-    # system "mv capt0000.jpg ~/Dropbox/Public/Tumblr/Upload/"
+    system "killall PTPCamera"
+    system "gphoto2 --capture-image-and-download"
+
+    @picture = Picture.new(:url => "efwefwe")
+    @picture.save
+
+    Dir.mkdir("public/pictures/#{@picture.id}")
+    system "mv capt0000.jpg public/pictures/#{@picture.id}/"
 
     # Authenticate via OAuth
     client = Tumblr::Client.new({
@@ -21,8 +25,10 @@ class HardWorker
       :oauth_token_secret => 'm2cTd3bRffC5cJ1VVhHPuYYKFbEuaGJp4awPycKnHMi0mGoKMe'
     })
 
+    picture_path = "public/pictures/#{@picture.id}/capt0000.jpg"
+
     # Make the request
-    client.photo("howyoureact-test.tumblr.com", {:data => ['/_____68_II_1440_900.jpg']})
+    client.photo("howyoureact-test.tumblr.com", {:data => [picture_path]})
 
 
   end
